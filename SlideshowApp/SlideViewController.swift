366a1b076db32f29bba23b18a637343cad13f7bb
takumi.yamaoka <tk-y@docomo.ne.jp>
2020-06-22 11:19:00 +0900

    SlideshowAppを作った。

---
+++ SlideshowApp/SlideViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SlideViewController.swift
+//  SlideshowApp
+//
+//  Created by 山岡巧 on 2020/06/20.
+//  Copyright © 2020 takumi.yamaoka. All rights reserved.
+//
+
+import UIKit
+
+class SlideViewController: UIViewController {
+
+    @IBOutlet weak var SlideImage: UIImageView!
+    
+    let slideimage = 0
+    var selectedImg: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        SlideImage.image = selectedImg
+    }
+    
+    
+    @IBAction func backBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,12 +9,100 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var switchButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    
+    
+    let imageNameArray = ["man1.png","man2.png","man3.png","man4.png","man5.png"]
+    var dispImageNo = 0
+    var timer: Timer!
 
+    @IBAction func backButton(_ sender: Any) {
+        dispImageNo -= 1
+        displayImage()
+    }
+    
+    @IBAction func nextButton(_ sender: Any) {
+        dispImageNo += 1
+        displayImage()
+    }
+    
+    
+    func displayImage() {
+        
+        if dispImageNo < 0 {
+            dispImageNo = 4
+        }
+        if dispImageNo > 4 {
+            dispImageNo = 0
+        }
+        
+        let name = imageNameArray[dispImageNo]
+        let image = UIImage(named: name)
+        imageView.image = image
+    }
+    
+    
+    
+    @IBAction func switchButton(_ sender: Any) {
+        if self.timer == nil {
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+            nextButton.isEnabled = false
+            backButton.isEnabled = false
+            switchButton.setTitle("停止", for: .normal)
+            self.view.backgroundColor = UIColor.lightGray
+            switchButton.titleLabel?.font = UIFont.systemFont(ofSize: 25)
+        } else if self.timer != nil {
+            self.timer.invalidate()
+            self.timer = nil
+            nextButton.isEnabled = true
+            backButton.isEnabled = true
+            switchButton.setTitle("再生", for: .normal)
+            self.view.backgroundColor = UIColor.white
+            switchButton.titleLabel?.font =
+            UIFont.systemFont(ofSize: 20)
+        }
+    }
+    
+    @objc func updateTimer(_ timer: Timer) {
+        if dispImageNo < imageNameArray.count - 1 {
+            dispImageNo += 1
+            displayImage()
+            print(dispImageNo)
+        } else {
+            dispImageNo = 0
+            displayImage()
+            print(dispImageNo)
+        }
+    }
+    
+    @IBAction func onTapAction(_ sender: Any) {
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let SlideViewController:SlideViewController = segue.destination as! SlideViewController
+        let name = imageNameArray[dispImageNo]
+        let image = UIImage(named: name)
+        SlideViewController.selectedImg = image
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let image = UIImage(named: "man1.png")
+        imageView.image = image
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
+        
+    }
 
 }
 
